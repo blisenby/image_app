3b90fb4cbabddabc731364e80a3cc0d738a694c9
blisenby <145783779+blisenby@users.noreply.github.com>
2024-04-25 09:13:35 -0400

    Add files via upload

---
+++ geeks_site/geeks_site/urls.py
@@ -0,0 +1,44 @@
+"""
+URL configuration for geeks_site project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+        
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from image_app.views import display_hotel_images, hotel_image_view,success
+
+urlpatterns = [
+	path('image_upload', hotel_image_view, name='image_upload'),
+	path('success', success, name='success'),
+    path('hotel_images', display_hotel_images, name = 'hotel_images'),
+]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL,
+						document_root=settings.MEDIA_ROOT)
